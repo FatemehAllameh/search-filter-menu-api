f89778ac674cd73c6e1868790b0f85586475df18
FatemehAllameh <fatemeh.allameh19@gmail.com>
2025-09-05 21:59:36 +0330

    Add error handling for API requests
    Implemented error handling when fetching data from API using Axios.
    Added error message display in case of network issues or failed requests.

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 // ELEMENTS
 const menuList = document.querySelector(".menu");
+const loading = document.querySelector(".loading");
+const errorText = document.querySelector(".err-text");
 
 let menuItems = null;
 
 // FETCHING DATA FROM API
-const getItems = async() => {
-    const response = await axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s");
+const getItems = async () => {
+  try {
+    const response = await axios.get(
+      "https://wwww.themealdb.com/api/json/v1/1/search.php?s"
+    );
     const data = await response.data;
     menuItems = data.meals;
-    displayMenuItems();  
-}
+    // HIDE LOADING TEXT
+    loading.style.display = "none";
+    displayMenuItems(menuItems);
+  } catch (err) {
+    // HIDE LOADING TEXT
+    loading.style.display = "none";
+    // SHOW ERROR MESSAGE
+    errorText.style.display = "block";
+    errorText.textContent = `${err.message}`;
+  }
+};
 
+// DISPLAY MENU ITEMS
 const displayMenuItems = () => {
-    menuItems.map((item) => {
-        const cardItem = `
+  menuItems.map((item) => {
+    const cardItem = `
         <div class="menu-item">
             <img src="${item.strMealThumb}" class="item-img" alt="${item.strMeal}">
             <h3 class="item-name">${item.strMeal}</h3>
         </div>
         `;
-        menuList.innerHTML += cardItem;
-    })
-}
+    menuList.innerHTML += cardItem;
+  });
+};
 
 getItems();
